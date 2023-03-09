e7268cdb27c4cdc8a7d3cfcfefcfd3c21ba8e1de
Ondřej Žára <ondrej.zara@gmail.com>
2023-03-09 18:04:01 +0100

    keyboard

--- js/app.js
+++ js/app.js
@@ -2,18 +2,25 @@ import sample from "./sample.js";
 import { parse, partition } from "./parser.js"
 import { formatGroup } from "./format.js"
 import { clear } from "./log.js";
+import * as keyboard from "./keyboard.js";
 
 
-async function go() {
+const TYPES = ["default", "srv", "camera", "ui", "fss", "dss"];
+
+async function go(str) {
 	clear();
-	let commands = parse(sample);
+	let commands = parse(str);
+	keyboard.validate(commands);
+
 	let partitioned = partition(commands);
 
 	let parent = document.querySelector("#groups");
 	parent.innerHTML = "";
 
-	let groups = ["default", "srv", "camera", "ui", "fss", "dss"].map(name => formatGroup(partitioned, name));
+	let groups = TYPES.map(name => formatGroup(partitioned, name));
 	parent.append(...groups);
+
+	keyboard.show(partitioned, TYPES);
 }
 
 async function onFile(e) {
@@ -29,7 +36,9 @@ async function onFile(e) {
 }
 
 function init() {
+	keyboard.hide();
 	document.querySelector("[type=file]").addEventListener("change", onFile);
+	go(sample);
 }
 
 init();
---
+++ js/keyboard.js
@@ -0,0 +1,87 @@
+import { log } from "./log.js";
+import conf from "./conf.js";
+
+
+let node = document.querySelector("#keyboard");
+let keys = [...node.querySelectorAll("kbd")];
+let form = node.querySelector("form");
+let data = {};
+
+function getKeyValue(key) {
+	return key.dataset.key || key.textContent;
+}
+
+function createBackground(hues) {
+
+}
+
+function applyColorsToKey(key, types) {
+	let keyValue = getKeyValue(key);
+
+	let usedTypes = types.filter(type => {
+		let commands = data[type];
+		return Object.values(commands).some(keys => {
+			return keys.some(key => {
+				return key.split("+").includes(keyValue);
+			});
+		});
+	});
+
+	if (usedTypes.length == 0) {
+		key.style.backgroundImage = "";
+		return;
+	} else {
+		let hues = usedTypes.map(type => conf[type].hue);
+		key.style.backgroundImage = createBackground(hues);
+	}
+}
+
+function applyColors() {
+	let types = [...form.querySelectorAll(":checked")].map(input => input.name);
+	keys.forEach(key => applyColorsToKey(key, types));
+}
+
+function buildCheckbox(type) {
+	let label = document.createElement("label");
+	label.style.setProperty("--hue", conf[type].hue);
+
+	let cb = document.createElement("input");
+	cb.type = "checkbox";
+	cb.checked = true;
+	cb.name = type;
+
+	label.append(cb, conf[type].label);
+	label.addEventListener("click", applyColors);
+	return label;
+}
+
+export function hide() {
+	node.hidden = true;
+}
+
+export function validate(commands) {
+	let values = new Set();
+	keys.forEach(key => values.add(getKeyValue(key)));
+
+	Object.entries(commands).forEach(entry => {
+		entry[1].forEach(key => {
+			key.split("+").forEach(key => {
+				if (values.has(key)) { return; }
+				if (key.match(/mouse/i)) { return; }
+				log("keyboard does not know the key", key, "for command", entry[0]);
+			});
+		});
+	});
+	log("keyboard validation complete")
+}
+
+export function show(partitioned, types) {
+	data = partitioned;
+
+	node.hidden = false;
+	form.innerHTML = "";
+
+	let inputs = types.map(buildCheckbox);
+	form.append(...inputs);
+	applyColors();
+}
